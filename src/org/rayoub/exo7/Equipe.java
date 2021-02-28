9faae4633649b98006859a7f55f41438fe412cfe
Rayane <chikhi.rayouna@gmail.com>
2021-02-28 23:44:57 +0100

    Add all classes

---
+++ src/org/rayoub/exo7/Equipe.java
@@ -0,0 +1,87 @@
+package org.rayoub.exo7;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.*;
+
+/* Q2 */
+
+public class Equipe{
+
+	private List<Joueur> joueurs;
+	private Iterator<Joueur> iterator;
+	
+Collection<Joueur>equipe = new ArrayList<>();	
+	  
+	  public Equipe() {
+	    joueurs = new LinkedList<Joueur>();
+	    iterator = null;
+	  } 
+
+/* Q3 : addJoueur(), removeJoueur(), isJoueurPresent() */
+     // a- addJoueur() //
+	  
+public void addJoueur(Joueur j) {
+		
+  System.out.println(j+" est un nouveau joueur");
+  joueurs.add(j);
+
+}
+
+     // b- removeJoueur() //
+
+public void removeJoueur(Joueur j) {
+	
+	  System.out.println(j+" est un joueur enlevé");
+	  joueurs.remove(j);
+
+  
+}
+
+
+     //c- containsJoueur() //
+
+public void containsJoueur(Joueur j) {
+	
+	  System.out.println(j+" est un joueur contenu dans la liste");
+	  joueurs.contains(j);
+
+
+}
+
+
+    // d- isJoueurPresent() //
+
+@SuppressWarnings("unchecked")
+public boolean isJoueurPresent(Joueur j) {
+	if (((List<Joueur>) j).isEmpty())
+		return false;
+	else 
+		return true;
+}    
+
+
+/* Q7: clear() */
+
+public void clear() {
+	equipe.clear();
+}
+
+/* Q8: getNombreJoueur() */
+
+public int getNombreJoueurs() {
+	return this.equipe.size();
+}
+
+/* Q9: getMoyenneAge() */
+
+public int getMoyenneAge() {
+	int somme = 0;
+	for(Joueur element : equipe)
+	somme = element.getAge() + somme;
+
+return somme/this.equipe.size();
+}
+
+}
---
+++ src/org/rayoub/exo8/Joueur.java
@@ -0,0 +1,49 @@
+package org.rayoub.exo8;
+
+public class Joueur {
+
+	/* Q1 : Classe Joueur possédant les propriétés ; nom, String et année de naissance */
+         /* membres attributs */
+	
+	private String nom;
+	private int anneeDeNaissance;
+
+	public Joueur(String nom, int anneeDeNaissance) {
+		super();
+		this.nom = nom;
+		this.anneeDeNaissance = anneeDeNaissance;
+	}
+
+        /* getters et setters */
+	
+ 	public String getNom() {
+		return nom;
+	}
+
+	/**
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	/**
+	 * @return the anneeDeNaissance
+	 */
+	public int getAnneeDeNaissance() {
+		return anneeDeNaissance;
+	}
+
+	/**
+	 * @param anneeDeNaissance the anneeDeNaissance to set
+	 */
+	public void setAnneeDeNaissance(int anneeDeNaissance) {
+		this.anneeDeNaissance = anneeDeNaissance;
+	}
+
+	public String getAnneeDeNaissance1() {
+		
+		return null;
+	}
+
+}
